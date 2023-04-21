95b4a126a4b697b55372294ebe2461167b776af0
SuShruthi <shruthacm@gmail.com>
2023-04-21 11:57:42 -0700

    add connection to the server

---
+++ play.js
@@ -0,0 +1,22 @@
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "172.19.233.129",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // add event listener for incoming data
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
